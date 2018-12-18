bce5f8583712ada0b5c974011ab37979314c2ebd
unknown <sachin@mobilenerd.net>
2018-12-19 00:08:01 +0530

    initial commit with content

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './App.css';
+import SocialShare from './social-share/SocialShare'
+class App extends Component {
+  render() {
+    const url = "https://www.github.com";
+    const title = "React share simplified";
+    return (
+      <div className="App">
+        <div className="align-inline" style={{marginTop:'10px'}}>
+          <span className="share-title">Share: </span>
+          <SocialShare
+            url={url}
+            title={title}
+            facebook={true}
+            facebookClass="iconStyle"
+            googlePlus={true}
+            googlePlusClass="iconStyle"
+            twitter={true}
+            twitterClass="iconStyle"
+            email={true}
+            emailClass="iconStyle"
+            whatsapp={true}
+            whatsappClass="iconStyle"
+            iconSize={32}
+            addClass="align-inline"
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/social-share/SocialShare.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import {
+  FacebookShareButton,
+  GooglePlusShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+  EmailShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  GooglePlusIcon,
+  EmailIcon,
+  WhatsappIcon
+} from "react-share";
+import './SocialShare.css';
+
+class SocialShare extends Component {
+    constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+  render() {
+    const {url,title,facebook,googlePlus,twitter,whatsapp,email,addClass,Previewemail} = this.props;
+    let {facebookClass,twitterClass,googlePlusClass,whatsappClass,emailClass} = this.props || "shareIcon";
+    console.log("iconsize",this.props.iconSize);
+    let iconSize = 32;
+    if(this.props.iconSize!==undefined){
+        iconSize = this.props.iconSize
+    }
+    return (
+      <span className={addClass}>
+        {facebook && (<FacebookShareButton url={url} quote={title} style={{outline:'none'}} className={facebookClass}>
+          <FacebookIcon
+            size={iconSize}
+            round />
+        </FacebookShareButton>)}
+        
+        {googlePlus && (<GooglePlusShareButton url={url} quote={title} style={{outline:'none'}} className={googlePlusClass}>
+          <GooglePlusIcon
+            size={iconSize}
+            round />
+        </GooglePlusShareButton>)}
+        
+        {twitter && (<TwitterShareButton url={url} quote={title} style={{outline:'none'}}  className={twitterClass}>
+          <TwitterIcon
+            size={iconSize}
+            round />
+        </TwitterShareButton>)}
+        
+        {whatsapp && (<WhatsappShareButton url={url} quote={title} style={{outline:'none'}} className={whatsappClass}>
+          <WhatsappIcon
+            size={iconSize}
+            round />
+        </WhatsappShareButton>)}
+        
+        {email && (<EmailShareButton url={url} quote={title} style={{outline:'none'}} className={emailClass}>
+          <EmailIcon
+            size={iconSize}
+            round />
+        </EmailShareButton>)}
+        {Previewemail && (<EmailShareButton url={url} quote={title} style={{outline:'none'}} className={emailClass}>
+          <EmailIcon
+            size={iconSize}
+            round />
+        </EmailShareButton>)}
+      </span>
+    );
+  }
+}
+export default SocialShare;
