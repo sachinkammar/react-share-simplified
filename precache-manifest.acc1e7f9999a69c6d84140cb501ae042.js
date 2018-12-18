d24e45d8925cd5b9a57b1e16fe56e90e81b6889a
unknown <sachin@mobilenerd.net>
2018-12-19 00:38:06 +0530

    Updates

---
+++ precache-manifest.acc1e7f9999a69c6d84140cb501ae042.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d1d0774c77401d3e7a57",
+    "url": "/static/js/main.d1d0774c.chunk.js"
+  },
+  {
+    "revision": "8e894850742415a26c43",
+    "url": "/static/js/1.8e894850.chunk.js"
+  },
+  {
+    "revision": "d1d0774c77401d3e7a57",
+    "url": "/static/css/main.f85fa1be.chunk.css"
+  },
+  {
+    "revision": "3113fcd0846654ef1af1e4d973a8dfea",
+    "url": "/index.html"
+  }
+];
